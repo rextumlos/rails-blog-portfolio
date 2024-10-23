d4f9e165a8a71a2b7b850091b75686e82890576b
rextumlos <jrptumlos@gmail.com>
2024-10-24 02:24:54 +0800

    add homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
 module.exports = {
-  content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
+    content: [
+        './app/views/**/*.html.erb',
+        './app/helpers/**/*.rb',
+        './app/assets/stylesheets/**/*.css',
+        './app/javascript/**/*.js'
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'Helvetica', 'Arial', 'sans-serif'],  // Add custom font for sans
+                header: ['Arimo', 'sans-serif'],                 // Example of a custom local font
+            },
+            fontSize: {
+                'xs': '0.75rem',      // Extra Small
+                'sm': '0.875rem',     // Small
+                'base': '1rem',       // Base
+                'lg': '1.125rem',     // Large
+                'xl': '1.25rem',      // Extra Large
+                '2xl': '1.5rem',      // 2X Large
+                '3xl': '1.875rem',    // 3X Large
+                '4xl': '2.25rem',     // 4X Large
+                '5xl': '3rem',        // 5X Large
+                '6xl': '4rem',        // 6X Large
+            },
+            colors: {
+                primary: {
+                    light: '#10b981',
+                    DEFAULT: '#00742e',
+                    dark: '#022c1b',
+                }
+            },
+        },
+    },
 }
