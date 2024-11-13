408ae3f98a43bb2a064e5ef97165a15089340740
rextumlos <jrptumlos@gmail.com>
2024-11-13 13:14:26 +0800

    Feature: fix #9 add about page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,89 +1,74 @@
 class HomeController < ApplicationController
   def index
-    @technical_skills = {
-      title: "TECHNICAL SKILLS",
-      data: [
-        {
-          title: "BACKEND DEVELOPMENT",
-          skills: ["Ruby on Rails", "ExpressJS", "Python Tornado", "REST API"]
-        },
-        {
-          title: "FRONTEND DEVELOPMENT",
-          skills: ["React JS, ERB, Tailwind CSS, Bootstrap"]
-        },
-        {
-          title: "DATABASES",
-          skills: ["MySQL, PostgreSQL", "MongoDB", "Firebase Storage"]
-        },
-        {
-          title: "BASIC PROGRAMMING",
-          skills: ["Python, C, C++, C#"]
-        },
-        {
-          title: "TOOLS",
-          skills: ["Git, Postman, WordPress, Docker, Figma"]
-        }
-      ]
-    }
-
-    @educations = {
-      title: "EDUCATION",
-      data: [
-        {
-          title: "Bachelor Of Science in Computer Engineering",
-          educations: [
-            {
-              school: "Polytechnic University of the Philippines - Manila",
-              date: "2019 - 2023"
-            }
-          ]
-        }
-      ]
-    }
-
-    @achievements = {
-      title: "ACHIEVEMENTS",
-      data: [
-        {
-          title: "PRESIDENT'S LISTER",
-          achievements: [
-            {
-              date: "2023"
-            }
-          ]
-        },
-        {
-          title: "DOST SEI SCHOLAR 2019",
-          achievements: [
-            {
-              date: "2019 - 2023"
-            }
-          ]
-        },
-      ]
-    }
-
-    @experiences = {
-      title: "EXPERIENCE",
-      data: [
-        {
-          title: "Software Engineer (ON-SITE)",
-          experiences: [
-            {
-              company: "Secure Smarter Services Inc",
-              date: "September 2023 - Present",
-              descriptions: [
-                "Develop and maintain robust, scalable web applications using Ruby on Rails and Python, ensuring high performance and seamless user experience.",
-                "Integrated third-party platforms including payment gateways and gaming services, enhancing system interoperability and client satisfaction.",
-                "Collaborated with clients to analyze requirements, delivering custom web service solutions aligned with business goals and technical specifications.",
-                "Created tailored business solutions for APIs, optimizing data flow and communication between external systems and internal services to meet operational demands and client objectives.",
-                "Designed and implemented dynamic, responsive pages using WordPress, improving the user interface, SEO, and overall site functionality."
-              ]
-            }
-          ]
-        }
-      ]
-    }
-
+    @techs = [
+      {
+        icon: "devicon-ruby-plain",
+        title: "Ruby"
+      },
+      {
+        icon: "devicon-rails-plain",
+        title: "Ruby on Rails"
+      },
+      {
+        icon: "devicon-html5-plain",
+        title: "HTML5"
+      },
+      {
+        icon: "devicon-css3-plain",
+        title: "CSS3"
+      },
+      {
+        icon: "devicon-javascript-plain",
+        title: "JavaScript"
+      },
+      {
+        icon: "devicon-react-plain",
+        title: "React"
+      },
+      {
+        icon: "devicon-python-plain",
+        title: "Python"
+      },
+      {
+        icon: "devicon-tailwindcss-plain",
+        title: "Tailwindcss"
+      },
+      {
+        icon: "devicon-bootstrap-plain",
+        title: "Bootstrap"
+      },
+      {
+        icon: "devicon-mysql-plain",
+        title: "MySQL"
+      },
+      {
+        icon: "devicon-mongodb-plain",
+        title: "MongoDB"
+      },
+      {
+        icon: "devicon-firebase-plain",
+        title: "Firebase"
+      },
+      {
+        icon: "devicon-git-plain",
+        title: "Git"
+      },
+      {
+        icon: "devicon-postman-plain",
+        title: "Postman"
+      },
+      {
+        icon: "devicon-wordpress-plain",
+        title: "WordPress"
+      },
+      {
+        icon: "devicon-docker-plain",
+        title: "Docker"
+      },
+      {
+        icon: "devicon-figma-plain",
+        title: "Figma"
+      }
+    ]
   end
 end
